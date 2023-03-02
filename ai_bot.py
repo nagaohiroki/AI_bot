0ec972ae1dcb980a418ca56a073cce6a8fcb83ed
nagaohiroki <testplayers0226@gmail.com>
2023-03-02 13:22:05 +0900

    use chat gpt-3.5-turbo

--- ai_bot.py
+++ ai_bot.py
@@ -6,12 +6,15 @@ import openai
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 chat_context = {}
+chat_messages = []
 
 
 @client.event
 async def on_ready():
     openai.api_key = os.environ['OPENAI_API_KEY']
     print('AI bot login')
+    chat_messages.append({"role": "system", "content": "discord bot"})
+            
 
 
 @client.event
@@ -29,7 +32,7 @@ async def on_message(message):
         if tag in message.clean_content:
             text = create_ai_image(message.clean_content.replace(tag, ''))
         else:
-            text = create_ai_chat(message)
+            text = create_gpt_chat(message)
     except Exception as e:
         text = str(e)
         global chat_context
@@ -53,7 +56,7 @@ def create_ai_chat(message):
 
 def create_ai_text(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-003",
+        engine="gpt-3.5-turbo",
         prompt=prompt,
         max_tokens=1024,
         temperature=0.7)
@@ -65,6 +68,20 @@ def create_ai_image(prompt):
     response = openai.Image.create(prompt=prompt, n=1, size="512x512")
     print(response)
     return response.data[0]['url']
+
+
+def create_gpt_chat(message):
+    context_text = chat_context.get(message.guild.id)
+    if not context_text:
+        context_text = ''
+    prompt = f'{message.author.name}:{message.clean_content}'
+    chat_messages.append({'role': 'user', 'content': prompt})
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages = chat_messages)
+    new_message = response.choices[0].message.content
+    chat_messages.append({'role': 'assistant', 'content': new_message})
+    return new_message.replace('Bot: ', '')
 
 
 client.run(os.environ['AI_BOT_TOKEN'])
